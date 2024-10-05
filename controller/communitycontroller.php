dc35acb6c7f7f762ea63fed5dde935c0a62ff0da
OlDeuS <66070083@kmitl.ac.th>
2024-10-06 01:15:59 +0700

    add class community controller (but not finish)

---
+++ controller/communitycontroller.php
@@ -0,0 +1,32 @@
+<?php
+class communitycontroller {
+
+    public function create_commu($name,$enrollkey,$ta_id,$tag){
+
+    }
+
+    public function delete_commu($id){
+
+    }
+
+    public function edit_commu($name,$amout,$enrollkey,$tag){
+
+    }
+
+    public function cancel_edit(){
+
+    }
+
+    public function confirm_edit(){
+
+    }
+
+    public function update($subject){
+
+    }
+
+    public function check_field(){
+
+    }
+}
+?>
