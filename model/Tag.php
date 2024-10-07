52e61dee0e2ea55b0253b918e831bb51c123eb7f
Teetat <66070092@kmitl.ac.th>
2024-10-07 23:05:22 +0700

    Modify Tag test

--- model/Tag.php
+++ model/Tag.php
@@ -4,11 +4,12 @@ class Tag{
     public $tag_name;
     public $tag_description;
     public $tag_createdate;
-    public function public function __construct($tag_id, $tag_name, $tag_description, $tag_createdate) {
+    public function __construct($tag_id, $tag_name, $tag_description, $tag_createdate) {
         $this->tag_id = $tag_id;
         $this->tag_name = $tag_name;
         $this->tag_description = $tag_description;
         $this->tag_createdate = $tag_createdate;
     }
+
 }
 ?>
