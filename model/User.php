cbe71c3f525e046a01da61d6d336b825ccada7bd
zedl3all <sanguanwongtanapat@gmail.com>
2024-10-07 21:31:44 +0700

    Add construct

--- model/User.php
+++ model/User.php
@@ -7,5 +7,15 @@ class User{
     public $email;
     public $role;
     public $createdate;
+
+    public function __construct($user_id, $firstname, $last_name, $username, $email, $role, $createdate) {
+        $this->user_id = $user_id;
+        $this->firstname = $firstname;
+        $this->last_name = $last_name;
+        $this->username = $username;
+        $this->email = $email;
+        $this->role = $role;
+        $this->createdate = $createdate;
+    }
 }
 ?>
