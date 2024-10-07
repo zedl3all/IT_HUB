7de008cee3c97d1278fa3cff843426d155d00a29
Teetat <66070092@kmitl.ac.th>
2024-10-07 23:00:31 +0700

    Build Tag Test

---
+++ model/Tag.php
@@ -0,0 +1,14 @@
+<?php
+class Tag{
+    public $tag_id;
+    public $tag_name;
+    public $tag_description;
+    public $tag_createdate;
+    public function public function __construct($tag_id, $tag_name, $tag_description, $tag_createdate) {
+        $this->tag_id = $tag_id;
+        $this->tag_name = $tag_name;
+        $this->tag_description = $tag_description;
+        $this->tag_createdate = $tag_createdate;
+    }
+}
+?>
