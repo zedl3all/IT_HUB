095cccc865ded83b5708743a94452de5c584d01c
Teetat <66070092@kmitl.ac.th>
2024-10-07 21:56:52 +0700

    Build  AnwerController

---
+++ controller/AnswerController.php
@@ -0,0 +1,22 @@
+<?php
+include '../model/Answer.php';
+class AnswerController{
+    private $question_id;
+    private $text;
+    private $mode;
+    private $answer;
+    private $community;
+
+    public function createAnswer($text, $mode){
+        if ($this->checkText($text)){
+            $this->answer = new Answer($text, $mode);
+        }
+    }
+    private function checkText($text){
+        return strcmp($text, "") != 0;
+    }
+    public function updateAns(){
+        $this->answer->setQuestion($this->question_id);
+    }
+}
+?>
