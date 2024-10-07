7c35337c729034804fc36b5fdb2d7f03d869b295
Teetat <66070092@kmitl.ac.th>
2024-10-07 20:37:48 +0700

    Build User test

---
+++ model/User.php
@@ -0,0 +1,14 @@
+<?php
+class User{
+    public $user_id;
+    public $firstname;
+    public $last_name;
+    public $username;
+    public $email;
+    public $role;
+    public $createdate;
+    public function public function __construct(Type $var = null) {
+        $this->var = $var;
+    }
+}
+?>
