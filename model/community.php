f286855db718ff24caf8be5afb098e5068477204
OlDeuS <66070083@kmitl.ac.th>
2024-10-13 02:47:09 +0700

    update community

--- model/community.php
+++ model/community.php
@@ -2,83 +2,118 @@
 class Community {
     private $id;
     private $name;
-    private $enrollkey;
-    private $ta_id;
-    private $c_amout;
-    private $c_user;
-    private $c_question;
-    private $datetime;
-    private $observer;
+    private $enroll;
+    private $tag;
+    private $amout;
+    private $owner;
+    private $subowner;
+    private $create_date;
+    private $description;
+    private $user;
+    private $question;
+    private $announcement;
 
     public function getID(){
         return $this->id;
     }
 
-    public function setID($newid){
-        $this->id = $newid;
+    public function setID($id){
+        $this->id = $id;
     }
 
     public function getName(){
         return $this->name;
     }
 
-    public function setName($newname){
-        $this->name = $newname;
+    public function setName($name){
+        $this->name = $name;
     }
 
     public function getEnroll(){
         return $this->enrollkey;
     }
 
-    public function setEnroll($newenroll){
-        $this->enrollkey = $newenroll;
+    public function setEnroll($enroll){
+        $this->enroll = $enroll;
     }
     
-    public function getTA(){
-        return $this->ta_id;
+    public function getOwner(){
+        return $this->owner;
     }
 
-    public function setTA($newta){
-        $this->ta_id = $newta;
+    public function setOwner($owner){
+        $this->owner = $owner;
+    }
+
+    public function gettag(){
+        return $this->tag;
+    }
+
+    public function settag($tag){
+        $this->tag = $tag;
+    }
+
+    public function getdescription(){
+        return $this->description;
+    }
+
+    public function setdescription($description){
+        $this->description = $description;
+    }
+
+    public function getannouncement(){
+        return $this->announcement;
+    }
+
+    public function setannouncement($announcement){
+        $this->announcement = $announcement;
+    }
+
+    public function getquestion(){
+        return $this->question;
+    }
+
+    public function setquestion($question){
+        $this->question = $question;
+    }
+
+    public function getsubOwner(){
+        return $this->subowner;
+    }
+
+    public function setsubOwner($subowner){
+        $this->subowner = $subowner;
+    }
+
+    public function getDate(){
+        return $this->create_date;
+    }
+
+    public function setDate($create_date){
+        $this->create_date = $create_date;
+    }
+
+    public function getUser(){
+        return $this->user;
+    }
+
+    public function setUser($user){
+        $this->user = $user;
     }
 
     public function checkEnrollkey($enrollkey){
 
     }
 
-    public function addUser($user){
+    public function insertUser($c_user){
 
     }
 
-    public function getCommunityByID($cid){
+    public function getCommunityByID($id){
 
     }
 
     public function getCommunity(){
-
-    }
-
-    public function updateCommunity(){
-
-    }
-
-    public function addObserver($observer){
-
-    }
-
-    public function removeObserver($observer){
-
-    }
-
-    public function notifyObserver(){
-
-    }
-
-    public function getQuestion($qid){
-
-    }
-
-    public function getQfromAns($aid){
 
     }
 }
