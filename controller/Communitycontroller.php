5fcd412c06965ab78d70b5b99ad6283a1e1c22ae
OlDeuS <66070083@kmitl.ac.th>
2024-10-13 02:29:23 +0700

    update CommunityController

--- controller/Communitycontroller.php
+++ controller/Communitycontroller.php
@@ -1,32 +1,29 @@
 <?php
+include '../model/Community.php';
+include '../controller/Community_Sql_controller.php';
 class CommunityController {
+    private $community;
+    private $sqlcommu = new Community_Sql_controller;
 
-    public function create_commu($name,$enrollkey,$ta_id,$tag){
-
-    }
-
-    public function delete_commu($id){
-
-    }
-
-    public function edit_commu($name,$amout,$enrollkey,$tag){
+    public function create_commu($name, $enroll, $description, $owner, $tag){
+        $this->$sqlcommu->createCommunity($name, $description, $owner);
+        $community = $this->$sqlcommu->getCommunityByLast($owner);
+        $this->$sqlcommu->addenrollkey($community, $enroll);
+        $this->$sqlcommu->addtag($community, $enroll);
 
     }
 
-    public function cancel_edit(){
+    public function delete_commu($community){
+        $this->$sqlcommu->deleteCommunity($community);
+    }
+
+    public function edit_commu($name, $description, $tag){
 
     }
 
-    public function confirm_edit(){
-
+    public function insertSubOwner($community, $subowner){
+        $this->$sqlcommu->insertsubOwner($community, $subowner);
     }
 
-    public function update($subject){
-
-    }
-
-    public function check_field(){
-
-    }
 }
 ?>
