1005e1120cfd2788b87f55181c5195d39cc5ff45
Teetat <66070092@kmitl.ac.th>
2024-10-14 20:31:38 +0700

    Refactor Tag

--- model/Tag.php
+++ model/Tag.php
@@ -1,38 +1,19 @@
 <?php
 class Tag{
-    private $tag_id;
     private $tag_name;
-    private $tag_description;
-    private $tag_createdate;
-    private function __construct($tag_id, $tag_name, $tag_description, $tag_createdate) {
-        $this->tag_id = $tag_id;
-        $this->tag_name = $tag_name;
-        $this->tag_description = $tag_description;
-        $this->tag_createdate = $tag_createdate;
-    }
-    public function getID(){
-        return $this->tag_id;
-    }
-    public function setID($tag_id){
-        $this->tag_id = $tag_id;
-    }
-    public function getName(){
+    private $tag_id;
+
+    public function getTagName(){
         return $this->tag_name;
     }
-    public function setName($tag_name){
+    public function setTagName($tag_name){
         $this->tag_name = $tag_name;
     }
-    public function getDescription(){
-        return $this->tag_description;
-    }
-    public function setDescription($tag_description){
-        $this->tag_description = $tag_description;
-    }
-    public function getCreateDate(){
-        return $this->tag_createdate;
-    }
-    public function setCreateDate($tag_createdate){
-        $this->tag_createdate = $tag_createdate;
+    public function getTagID(){
+        return $this->tag_id;
+    }   
+    public function setTagID($tag_id){
+        $this->tag_id = $tag_id;
     }
 }
 ?>
