c7199eeb0893cd4d0e1a9fcb81bcc0358e2e980b
zedl3all <sanguanwongtanapat@gmail.com>
2024-10-17 17:08:17 +0700

    Remove unused interfaces and classes related to questions and answers

--- controller/IQuestionAccess.php
+++
@@ -1,8 +0,0 @@
-<?php
-interface IQuestionAccess{
-    public function createQuestion($user, $commu, $q_id, $header, $desciption);
-    public function deleteQuestion($question);
-    public function createAnswer($text, $mode, $question, $user);
-    public function deleteAnswer($answer);
-}
-?>
